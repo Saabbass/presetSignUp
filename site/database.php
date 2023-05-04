7e0581cf50fe68476d2eb95a39fb23aed5a57e56
Saabbass <178414@novacollege.nl>
2023-05-04 19:52:14 +0200

    database.php commentaar toevoeging

--- site/database.php
+++ site/database.php
@@ -1,10 +1,12 @@
 <?php
 // Database configuratie
+// Database configuration
 $host  = "mariadb";
 $dbuser = "root";
 $dbpass = "password";
 $dbname = "members";
 
-// Maak een  database connectie
+// Maak een atabase connectie
+// Connect to the database
 $conn = mysqli_connect($host, $dbuser, $dbpass, $dbname);
 ?>
