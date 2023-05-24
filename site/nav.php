046154780e7f97b0ac9ed66632fd7c6e548421f5
Saabbass <178414@novacollege.nl>
2023-05-24 21:16:30 +0200

    nav code verbetering

--- site/nav.php
+++ site/nav.php
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/x-icon" href="img/logoGimp.png">
-    <link rel="stylesheet" href="css/styles.css?v=<?php echo time(); ?>">
-</head>
-
-<body>
     <nav>
         <ul>
             <li>
@@ -21,6 +9,3 @@
         <a href="login.php">login</a>
         <button class="btn_contact">Contact</button>
     </nav>
-</body>
-
-</html>
