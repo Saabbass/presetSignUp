07292b198969d3d6454ac65c05fc45c84634c042
Saabbass <178414@novacollege.nl>
2023-05-24 21:13:07 +0200

    code paginas verbeteren

--- site/footer.php
+++ site/footer.php
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/x-icon" href="img/logoGimp.png">
-    <link rel="stylesheet" href="css/styles.css?v=<?php echo time(); ?>">
-</head>
-
-<body>
     <div class="footer_container">
         <div class="container">
             <div class="container_width">
@@ -21,6 +9,6 @@
             </div>
         </div>
     </div>
-</body>
+    </body>
 
-</html>
+    </html>
