b36e925d24e57b23090c108051a3748012ec5b55
Saabbass <178414@novacollege.nl>
2023-05-24 21:21:56 +0200

    signup pagina code verbetering

--- site/signUp.php
+++ site/signUp.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SignUp page</title>
-    <link rel="stylesheet" href="css/styles.css">
 </head>
 
 <body>
@@ -109,6 +102,3 @@
     ?>
     <!-- end footer -->
     <!-- einde footer -->
-</body>
-
-</html>
